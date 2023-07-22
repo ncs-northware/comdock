e6e323d348499102a61587110ae277fb60a40082
Oke <56229919+onissen@users.noreply.github.com>
2023-07-22 16:44:06 +0200

    No upload testing at index

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,9 @@
 import Layout from "@/components/common/Layout";
-import { fetcher } from "@/helpers/helpScripts";
 export default function Index() {
 
-  const upload = async (e) => {
-    e.preventDefault();
-    await fetcher(
-      'upload',
-      '',
-      {
-        method: 'post',
-        body: new FormData(e.target)
-      }
-    )
-    // FIXME: Later this function needs a reload of the page and a success Alert
-  }
-
-  return (
+ return (
     <Layout nopageHeader>
-      <form onSubmit={(e) => upload(e)}>
-        <input type="file" name="files" />
-        <input type="text" name="ref" value="api::cert-document.cert-document" />
-        <input type="text" name="refId" value="1" />
-        <input type="text" name="field" value="document" />
-        <input type="submit" value="Submit" className="btn btn-primary" />
-      </form>
+      <h1>Index Page</h1>
     </Layout>
   );
 }
