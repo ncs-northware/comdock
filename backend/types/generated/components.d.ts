02e262a7d082f0d1e45a7fc9ac5accd6d39bf658
onissen <56229919+onissen@users.noreply.github.com>
2024-08-25 14:36:36 +0200

    New Version cleanup

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,20 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface RelatedDocsRelatedDocs extends Schema.Component {
+  collectionName: 'components_related_docs_related_docs';
+  info: {
+    displayName: 'relatedDocs';
+    description: '';
+  };
+  attributes: {
+    document: Attribute.Media<'images' | 'files'> & Attribute.Required;
+    type: Attribute.Enumeration<
+      ['Unterschriftsnachweis', 'Apostille', 'Beglaubigung', 'Zertifizierung']
+    > &
+      Attribute.Required;
+  };
+}
+
 export interface PersonNetworkNetzwerkDerPerson extends Schema.Component {
   collectionName: 'components_person_network_netzwerk_der_people';
   info: {
@@ -31,21 +46,6 @@ export interface PersonNetworkNetzwerkDerPerson extends Schema.Component {
       'oneToOne',
       'api::hr-public.hr-public'
     >;
-  };
-}
-
-export interface RelatedDocsRelatedDocs extends Schema.Component {
-  collectionName: 'components_related_docs_related_docs';
-  info: {
-    displayName: 'relatedDocs';
-    description: '';
-  };
-  attributes: {
-    document: Attribute.Media<'images' | 'files'> & Attribute.Required;
-    type: Attribute.Enumeration<
-      ['Unterschriftsnachweis', 'Apostille', 'Beglaubigung', 'Zertifizierung']
-    > &
-      Attribute.Required;
   };
 }
 
@@ -122,17 +122,6 @@ export interface LeiHistoryLeiHistory extends Schema.Component {
   };
 }
 
-export interface CompaniesFurtherNamesVorherigeNamen extends Schema.Component {
-  collectionName: 'components_companies_further_names_vorherige_namen';
-  info: {
-    displayName: 'Vorherige Namen';
-  };
-  attributes: {
-    further_cname: Attribute.String & Attribute.Required;
-    name_upto: Attribute.Date;
-  };
-}
-
 export interface ExternalsExternals extends Schema.Component {
   collectionName: 'components_externals_externals';
   info: {
@@ -154,16 +143,27 @@ export interface ExternalsExternals extends Schema.Component {
   };
 }
 
+export interface CompaniesFurtherNamesVorherigeNamen extends Schema.Component {
+  collectionName: 'components_companies_further_names_vorherige_namen';
+  info: {
+    displayName: 'Vorherige Namen';
+  };
+  attributes: {
+    further_cname: Attribute.String & Attribute.Required;
+    name_upto: Attribute.Date;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'person-network.netzwerk-der-person': PersonNetworkNetzwerkDerPerson;
       'related-docs.related-docs': RelatedDocsRelatedDocs;
+      'person-network.netzwerk-der-person': PersonNetworkNetzwerkDerPerson;
       'network-persons.verbundene-personen': NetworkPersonsVerbundenePersonen;
       'network-companies.verbundene-unternehmen': NetworkCompaniesVerbundeneUnternehmen;
       'lei-history.lei-history': LeiHistoryLeiHistory;
-      'companies-further-names.vorherige-namen': CompaniesFurtherNamesVorherigeNamen;
       'externals.externals': ExternalsExternals;
+      'companies-further-names.vorherige-namen': CompaniesFurtherNamesVorherigeNamen;
     }
   }
 }
