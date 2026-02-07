bd1c6947d7c403623b8b715d41a637aef1965d9f
onissen <56229919+onissen@users.noreply.github.com>
2026-02-07 23:01:29 +0100

    feat: Add germanDate function for correct date string rendering as TT.MM.YYYY

--- payload/src/lib/utils.ts
+++ payload/src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function germanDate(dateString: string | number | Date) {
+  // Renders the given dateString to German format TT.MM.YYYY
+  const formatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  } as const;
+  const date = new Date(dateString).toLocaleDateString("de-DE", formatOptions);
+  return date;
+}
