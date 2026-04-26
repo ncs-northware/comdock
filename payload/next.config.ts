350554ddfcfa1a7bb8585a5b8a878fbbdf5936a9
onissen <56229919+onissen@users.noreply.github.com>
2026-04-26 12:14:15 +0200

    fix: transform next.config into a TypeScript file

--- payload/next.config.ts
+++ payload/next.config.ts
@@ -1,7 +1,7 @@
 import { withPayload } from "@payloadcms/next/withPayload";
+import type {NextConfig} from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   // Your Next.js config here
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
