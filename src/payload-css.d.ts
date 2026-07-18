3419e39ef2e495fc020887570a75f374a7161d91
onissen <56229919+onissen@users.noreply.github.com>
2026-07-18 20:16:28 +0200

    fix: Remove type declaration for @payloadcms/next/css
    Der Workaround einer manuellen Typdeklaration für @payloadcms/next/css ist nicht mehr nötig, da diese Deklaration im Package enthalten ist.

--- src/payload-css.d.ts
+++
@@ -1,8 +0,0 @@
-/***
- * This is a workaround file because the export @payloadcms/next/css has no type declaration export currently.
- * The Type Declaration is needed with TypeScript 6 to get successfull builds.
- * Payload works on this issue already: https://github.com/payloadcms/payload/pull/16348
- ***/
-// FIXME: When the Type Declaration is implemented in @payloadcms/next/css remove this file (Internal Issue #412)
-
-declare module "@payloadcms/next/css";
